983fe16918eecc759d2fe6547bed39860267358c
Bulutze <zzeynepcagil@gmail.com>
2025-02-27 15:12:42 +0300

    day 3 eklendi

---
+++ Day_003/TreasureIsland/main.py
@@ -0,0 +1,45 @@
+print('''  ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----\
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\
+ \_/__________________________________________________________________/''')
+
+print("Welcome to Treasure Island! ")
+print("Your mission to find to treasure.")
+cross_road =input('You\'re at a cross road. Where do you want to go? \n Type "left" or "right" ')
+if cross_road=="left":
+    lake=input('You\'ve come to a lake. There is an island in the middle of the lake. \n Type "wait" to wait for a boat. Type "swim" to swim across.')
+    if lake=="wait":
+       door= input("You arrive at the island unharmed. There is a house with 3 doors. \n One red, one yellow and one blue. Which colour do you choose?")
+       if door=="red":
+           print("Burned by fire. \n Game Over.")
+       elif door=="blue":
+           print("Eaten by beasts. \n Game Over.")
+       elif door=="yellow":
+           print("You win!")
+       else:
+           print("Game Over.")
+    elif lake=="swim":
+        print("You get attacked by an angry trout. Game Over.")
+if cross_road=="right":
+    print("You fell into a hole. Game Over.")
